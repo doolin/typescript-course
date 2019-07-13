805ad144362d77f8eff2bf6426d49dacfbeaa594
dave doolin <david.doolin@gmail.com>
2019-07-13 04:50:55 -0700

    video 15, annotations with variables

---
+++ features/annotations/variables.ts
@@ -0,0 +1,13 @@
+// let is used for demonstration of how types work,
+// one would normally use const here.
+let apples: number = 5;
+let speed: string = 'fast';
+let hasName: boolean = true;
+
+// name is same as type
+let nothingMuch: null = null;
+
+let nothing: undefined = undefined;
+
+// built in objects
+let now: Date = new Date();
