0f9ab311da2ac68a6b7f72dde4b09096f43fc991
dave doolin <david.doolin@gmail.com>
2019-07-24 06:12:54 -0700

    video 27, annotating functions from objects

--- features/annotations/functions.ts
+++ features/annotations/functions.ts
@@ -18,6 +18,21 @@ const logger = (message: string):void => {
   console.log(message);
 };
 
-const throwError = (message: string): never => {
-  throw new Error(message);
+//const throwError = (message: string): never => {
+  //if(!message) {
+  //  throw new Error(message);
+  //}
+//};
+
+const forecast = {
+  date: new Date(),
+  weather: 'sunny'
 };
+
+//const logWeather = (forecast: {date: Date, weather: string}): void => {
+const logWeather = ({date, weather}: {date: Date, weather: string}): void => {
+    console.log(date);
+  console.log(weather);
+};
+
+logWeather(forecast);
