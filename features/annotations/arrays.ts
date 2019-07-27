59b04f09fd9801437d37a52a8ac1c45c07353660
dave doolin <david.doolin@gmail.com>
2019-07-27 07:08:51 -0700

    video 30, typed arrays

--- features/annotations/arrays.ts
+++ features/annotations/arrays.ts
@@ -1,6 +1,20 @@
 const carMakers: string[] = ['ford', 'toyota', 'chevy'];
 const dates = [new Date(), new Date];
 
+// Help with inference when extracting values
+const car = carMakers[0];
+const myCar = carMakers.pop();
+
+// Prevent incompatible values
+// carMakers.push(100);
+
+// Help with `map`
+const cm = carMakers.map((car: string): string => {
+  return car.toUpperCase();
+});
+
+console.log(cm);
+
 const carsByMake: string[][] = [ 
   ['f150'],
   ['corolla'],
