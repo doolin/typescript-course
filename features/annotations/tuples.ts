9852ed67e03528995b70a3928deca2b424153ecc
dave doolin <david.doolin@gmail.com>
2019-07-29 04:32:56 -0700

    video 34, tuples and type alias

---
+++ features/annotations/tuples.ts
@@ -0,0 +1,16 @@
+const drink = {
+  color: 'brown',
+  carbonated: true,
+  sugar: 40
+};
+
+// const pepsi: [string, boolean, number] = ['brown', true, 40];
+
+// type alias
+type Drink = [string, boolean, number];
+
+const pepsi: Drink = ['brown', true, 40];
+const sprite: Drink = ['clear', true, 40];
+const tea: Drink = ['brown', false, 0];
+
+console.log(tea);
