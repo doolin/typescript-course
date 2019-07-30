ba554cac9697a66c347df949a6c2620d44fed133
dave doolin <david.doolin@gmail.com>
2019-07-30 06:12:35 -0700

    video 34, prefer objects over tuples

--- features/annotations/tuples.ts
+++ features/annotations/tuples.ts
@@ -14,3 +14,10 @@ const sprite: Drink = ['clear', true, 40];
 const tea: Drink = ['brown', false, 0];
 
 console.log(tea);
+
+const carSpecs: [number, number] = [400, 3354];
+
+const carStats = {
+  horsepower: 400,
+  weight: 3354
+};
