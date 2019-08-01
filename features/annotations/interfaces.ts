639a62da96f162c014fab292402c54568884b55e
dave doolin <david.doolin@gmail.com>
2019-08-01 04:04:10 -0700

    video 38, interface

--- features/annotations/interfaces.ts
+++ features/annotations/interfaces.ts
@@ -1,10 +1,17 @@
+interface Vehicle {
+  name: string;
+  year: number;
+  broken: boolean;
+}
+
 const oldCivic = {
   name: 'civiv',
   year: 2000,
   broken: true
 };
 
-const printVehicle = (vehicle: {name: string, year: number, broken: boolean}): void => {
+// const printVehicle = (vehicle: {name: string, year: number, broken: boolean}): void => {
+const printVehicle = (vehicle: Vehicle): void => {
   console.log(`name: ${vehicle.name}`);
   console.log(`year: ${vehicle.year}`);
   console.log(`broken: ${vehicle.broken}`);
