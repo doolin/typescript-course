2307d2eb10a8e0c5bd60c60a5198c04454aa04d5
dave doolin <david.doolin@gmail.com>
2019-08-02 04:34:03 -0700

    video 39, syntax around interrface

--- features/annotations/interfaces.ts
+++ features/annotations/interfaces.ts
@@ -2,12 +2,16 @@ interface Vehicle {
   name: string;
   year: number;
   broken: boolean;
+  summary(): string; 
 }
 
 const oldCivic = {
-  name: 'civiv',
+  name: 'civic',
   year: 2000,
-  broken: true
+  broken: true,
+  summary(): string {
+    return `Name: ${this.name}`
+  }
 };
 
 // const printVehicle = (vehicle: {name: string, year: number, broken: boolean}): void => {
@@ -15,6 +19,7 @@ const printVehicle = (vehicle: Vehicle): void => {
   console.log(`name: ${vehicle.name}`);
   console.log(`year: ${vehicle.year}`);
   console.log(`broken: ${vehicle.broken}`);
+  console.log(vehicle.summary());
 };
 
 printVehicle(oldCivic);
