0de93822c773f68b9b007ace210e46d0e39794ad
dave doolin <david.doolin@gmail.com>
2019-08-08 04:06:51 -0700

    video 46, fields in classes

--- features/annotations/classes.ts
+++ features/annotations/classes.ts
@@ -1,5 +1,14 @@
 class Vehicle {
-  private drive(): void {
+  // color: string; // = 'red';
+
+  // constructor(color: string) {
+  //   this.color = color;
+  // }
+  
+  constructor(public color: string) {
+  }
+
+  protected drive(): void {
     console.log('driving');
   }
 
@@ -9,19 +18,20 @@ class Vehicle {
 }
 
 class Car extends Vehicle {
-   private drive(): void {
-     console.log('car driving');
-   }
+   //private drive(): void {
+   //  console.log('car driving');
+   //}
 
    startDrivingProcess(): void {
      this.drive();
    }
 }
 
-const vehicle = new Vehicle();
-vehicle.drive();
+const vehicle = new Vehicle('orange');
+//vehicle.drive();
+console.log(vehicle.color);
 vehicle.honk();
 
-const car = new Car();
-car.startDrivingProcess();
-car.honk();
+//const car = new Car();
+//car.startDrivingProcess();
+//car.honk();
