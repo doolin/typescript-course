300a284d04cbc6580f5ed8d40a3c280e01b307c6
dave doolin <david.doolin@gmail.com>
2019-08-15 04:31:35 -0700

    video 56, defining a company

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,5 +1,9 @@
 import { User } from './User';
+import { Company } from './Company';
 
+const company = new Company();
 const user = new User();
 
-// console.log(user);
+
+console.log(user);
+console.log(company);
