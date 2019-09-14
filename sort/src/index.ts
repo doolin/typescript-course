f0a39a03d9b4591ec0affad8971eb10385f851c1
dave doolin <david.doolin@gmail.com>
2019-09-14 04:35:15 -0700

    video 82. extracted sorter class

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,15 +1,22 @@
 // compile with tsc index.ts
 // can run with tsc -w
 class Sorter {
-  constructor(public collection: number[] | string) {}
+  constructor(public collection: number[] | string | LinkedList) {}
 
   sort(): void {
     const { length } = this.collection;
 
-    // if collection is an array of numbers
-    if (this.collection instanceof Array) {
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
+
+        if (this.collection instanceof LinkedList) {
+          
+        }
+
+
+    // if collection is an array of numbers
+    if (this.collection instanceof Array) {
+
         if (this.collection[j] > this.collection[j+1]) {
           const temp = this.collection[j];
           this.collection[j] = this.collection[j+1];
@@ -20,6 +27,9 @@ class Sorter {
   }
     // if collection is a string
     // need logic for comparing and swapping characters
+    if (typeof this.collection === 'string') {
+      
+    }
   }
 }
 
