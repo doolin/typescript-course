c9b044e87f01994ffb0fc51364e892f62c0d21dc
dave doolin <david.doolin@gmail.com>
2019-09-18 03:50:48 -0700

    video 83 continues, super long

--- sort/src/NumbersCollection.ts
+++ sort/src/NumbersCollection.ts
@@ -1,6 +1,10 @@
 export class NumbersCollection {
   constructor(public data: number[]) {}
 
+  get length() {
+    return this.data.length;
+  }
+
   compare(leftIndex: number, rightIndex: number): boolean {
     return this.data[leftIndex] > this.data[rightIndex];
   }
