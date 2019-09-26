1c05d75ac76d548d82264d214ea16f15a7dcde23
dave doolin <david.doolin@gmail.com>
2019-09-26 06:03:12 -0700

    swap for strings

--- sort/src/CharactersCollection.ts
+++ sort/src/CharactersCollection.ts
@@ -8,4 +8,14 @@ export class CharactersCollection {
   compare(left: number, right: number): boolean {
     return this.data[left].toLowerCase() > this.data[right].toLowerCase();
   }
+
+  swap(left: number, right: number): void {
+    const characters = this.data.split('');
+
+    const leftChar = characters[left];
+    characters[left] = characters[right];
+    characters[right] = leftChar;
+
+    this.data = characters.join('');
+  }
 }
