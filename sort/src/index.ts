6ad06c81c7792df8377ec007cbf69f58a16dc147
dave doolin <david.doolin@gmail.com>
2019-09-27 03:51:35 -0700

    start of linked list implementation

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -2,8 +2,14 @@
 // can run with tsc -w
 import { Sorter } from './sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 
 const numbersCollection = new NumbersCollection([1,5,3,6,]);
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection('gsgsdb');
+const sorter2 = new Sorter(charactersCollection);
+sorter2.sort();
+console.log(charactersCollection.data);
