dc03802aa1c1a630518affd97f03b575b3f8203b
dave doolin <david.doolin@gmail.com>
2019-10-04 03:49:07 -0700

    sort the linked list

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -3,6 +3,7 @@
 import { Sorter } from './sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 const numbersCollection = new NumbersCollection([1,5,3,6,]);
 const sorter = new Sorter(numbersCollection);
@@ -13,3 +14,13 @@ const charactersCollection = new CharactersCollection('gsgsdb');
 const sorter2 = new Sorter(charactersCollection);
 sorter2.sort();
 console.log(charactersCollection.data);
+
+const linkedlist = new LinkedList();
+linkedlist.add(500);
+linkedlist.add(-10);
+linkedlist.add(-3);
+linkedlist.add(4);
+
+const sorter3 = new Sorter(linkedlist);
+sorter3.sort();
+linkedlist.print();
