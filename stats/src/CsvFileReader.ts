2fcf70e58780dfa644442154c2bee6a364795ad1
dave doolin <david.doolin@gmail.com>
2019-11-03 03:06:45 -0800

    video 108 row processing

--- stats/src/CsvFileReader.ts
+++ stats/src/CsvFileReader.ts
@@ -1,4 +1,6 @@
 import fs from 'fs';
+import { dateStringToDate } from './utitl';
+import { parse } from 'path';
 
 export class CsvFileReader {
   data: string[][] = [];
@@ -10,6 +12,17 @@ export class CsvFileReader {
       encoding: 'utf-8'
     }).split('\n').map((row: string): string[] => {
       return row.split(',');
+    })
+    .map((row: string[]) => {
+      return [
+        dateStringToDate(row[0]),
+        row[1],
+        row[2],
+        parseInt(row[3]),
+        parseInt(row[4]),
+        row[5],
+        row[6]
+      ];
     });
     
   }
