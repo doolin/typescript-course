3b911597061b5951a8599dafb384ee0a173bf9df
dave doolin <david.doolin@gmail.com>
2019-11-04 03:22:54 -0800

    video 109 enum type assertion

--- stats/src/CsvFileReader.ts
+++ stats/src/CsvFileReader.ts
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import { dateStringToDate } from './utitl';
 import { parse } from 'path';
+import { MatchResult } from './MatchResult';
 
 export class CsvFileReader {
   data: string[][] = [];
@@ -13,14 +14,14 @@ export class CsvFileReader {
     }).split('\n').map((row: string): string[] => {
       return row.split(',');
     })
-    .map((row: string[]) => {
+    .map((row: string[]): any => {
       return [
         dateStringToDate(row[0]),
         row[1],
         row[2],
         parseInt(row[3]),
         parseInt(row[4]),
-        row[5],
+        row[5] as MatchResult,
         row[6]
       ];
     });
