a0897a6a299b6adf23b0aeb37515e6ec0106cf87
dave doolin <david.doolin@gmail.com>
2019-11-08 03:39:58 -0800

    video 113, abstract class

--- stats/src/CsvFileReader.ts
+++ stats/src/CsvFileReader.ts
@@ -1,14 +1,14 @@
 import fs from 'fs';
-import { dateStringToDate } from './utitl';
-import { parse } from 'path';
 import { MatchResult } from './MatchResult';
 
 type MatchData = [Date, string, string, number, number, MatchResult, string];
 
-export class CsvFileReader {
+export abstract class CsvFileReader {
   data: MatchData[] = [];
 
   constructor(public filename: string) {}
+
+  abstract mapRow(row: string[]): MatchData;
 
   read(): void {
     this.data = fs.readFileSync(this.filename, {
@@ -19,17 +19,5 @@ export class CsvFileReader {
     //.map((row: string[]): (Date | number | string | MatchResult)[] => {
     // .map((row: string[]): any => {
     .map((this.mapRow);
-  }
-
-  mapRow(row: string[]): MatchData {
-    return [
-      dateStringToDate(row[0]),
-      row[1],
-      row[2],
-      parseInt(row[3]),
-      parseInt(row[4]),
-      row[5] as MatchResult,
-      row[6]
-    ];
   }
 }
