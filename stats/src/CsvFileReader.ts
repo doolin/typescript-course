69e4d4e0fec2c5dda5f5510970070b12ac96d9d5
dave doolin <david.doolin@gmail.com>
2019-11-11 04:07:41 -0800

    prepare for interface-based refactoring

--- stats/src/CsvFileReader.ts
+++ stats/src/CsvFileReader.ts
@@ -1,11 +1,14 @@
 import fs from 'fs';
+import { dateStringToDate } from './utitl';
+import { parse } from 'path';
+import { MatchResult } from './MatchResult';
 
-export abstract class CsvFileReader<T> {
-  data: T[] = [];
+type MatchData = [Date, string, string, number, number, MatchResult, string];
+
+export class CsvFileReader {
+  data: MatchData[] = [];
 
   constructor(public filename: string) {}
-
-  abstract mapRow(row: string[]): T;
 
   read(): void {
     this.data = fs.readFileSync(this.filename, {
@@ -15,6 +18,17 @@ export abstract class CsvFileReader<T> {
     })
     //.map((row: string[]): (Date | number | string | MatchResult)[] => {
     // .map((row: string[]): any => {
-    .map((this.mapRow);
+    .map((row: string[]): MatchData => {
+      return [
+      dateStringToDate(row[0]),
+      row[1],
+      row[2],
+      parseInt(row[3]),
+      parseInt(row[4]),
+      row[5] as MatchResult,
+      row[6]
+      ];
+    });
+    
   }
 }
