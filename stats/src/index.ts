455d5c60c1796c1bebb5003a5cff7db6e7b4a486
dave doolin <david.doolin@gmail.com>
2019-11-15 03:46:49 -0800

    video 120 interface implemented correctly

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,13 +1,18 @@
 import { MatchReader } from './MatchReader';
+import { CsvFileReader } from './CsvFileReader';
 import { MatchResult } from './MatchResult';
 
-const reader = new MatchReader('football.csv');
-reader.read();
+// const reader = new MatchReader('football.csv');
+// reader.read();
 
-console.log(reader.data);
+const csvFileReader = new CsvFileReader('football.csv');
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
+
+console.log(matchReader.matches);
 
 let manUnitedWins:number = 0;
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
