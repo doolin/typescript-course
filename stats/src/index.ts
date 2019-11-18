61bacf1ac97bc7b1106736de0a21a241c7ff0c12
dave doolin <david.doolin@gmail.com>
2019-11-18 03:58:44 -0800

    wins analysis class

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -11,13 +11,4 @@ matchReader.load();
 
 console.log(matchReader.matches);
 
-let manUnitedWins:number = 0;
-for (let match of matchReader.matches) {
-  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
-}
-
 console.log(`Man United won ${manUnitedWins} games`)
