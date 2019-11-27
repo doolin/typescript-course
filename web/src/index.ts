f657f62b5f5604573c06c340e51ab400e0415e9c
dave doolin <david.doolin@gmail.com>
2019-11-27 05:16:34 -0800

    video 141 User class

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,1 +1,7 @@
-console.log("Hi there!");
+// console.log("Hi there!");
+import { User } from './models/User';
+
+const user = new User({ name: 'foo', age: 20 });
+
+console.log(user.get('name'));
+console.log(user.get('age'));
