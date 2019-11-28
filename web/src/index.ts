d2afa10397ecdd0657ceb7f75b0bc025f1150363
dave doolin <david.doolin@gmail.com>
2019-11-28 03:57:40 -0800

    user set method

--- web/src/index.ts
+++ web/src/index.ts
@@ -3,5 +3,8 @@ import { User } from './models/User';
 
 const user = new User({ name: 'foo', age: 20 });
 
+user.set({ name: 'foobarski' });
+user.set({ age: 21 });
+
 console.log(user.get('name'));
 console.log(user.get('age'));
