4d4d309f6d4354fb19e018f7a6d95970bd1d8788
dave doolin <david.doolin@gmail.com>
2019-12-02 03:33:43 -0800

    video 147 add event trigger to user

--- web/src/index.ts
+++ web/src/index.ts
@@ -9,8 +9,15 @@ user.set({ age: 21 });
 console.log(user.get('name'));
 console.log(user.get('age'));
 
-user.on('change', () => {});
-user.on('change', () => {});
-user.on('no-change', () => {});
+user.on('change', () => {
+  console.log("change #1");
+});
+user.on('change', () => {
+  console.log("change #2");
+});
+user.on('no-change', () => {
+  console.log("no change");
+});
 
 console.log(user);
+user.trigger('change');
