46244787ccba627a605abdd9d13dcad5b9816b12
dave doolin <david.doolin@gmail.com>
2019-12-07 00:53:09 -0800

    not actually working at the moment

--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -45,4 +45,17 @@ export class User {
         this.set(response.data);
       });
   }
+
+  save(): void {
+    const id = this.get('id');
+
+    console.log(id);
+    console.log(this.data);
+
+    if (id) {
+      axios.put(`http://localhost:3000/users/${id}`, this.data);
+    } else {
+      axios.put('http://localhost:3000/users/', this.data);
+    }
+  }
 }
