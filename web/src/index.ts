db9c365e747f8ab83efcf3f18a1fb6ca08c4a42f
dave doolin <david.doolin@gmail.com>
2019-12-31 03:56:50 -0800

    167 - redo index.ts for new arch

--- web/src/index.ts
+++ web/src/index.ts
@@ -2,20 +2,4 @@ import { User } from './models/User';
 
 const user = new User({ id: 1 })
 
-// user.fetch();
-
-// setTimeout(() => {
-//  console.log(user);
-// }, 4000)
-
-user.set({ name: 'New Name', age: 43 });
-user.save();
-
-const user2 = new User({ name: "foobar", age: 0 })
-user2.save();
-
-user2.events.on('change', () => {
-  console.log('changed');
-});
-
-user2.events.trigger('change');
+user.sync.save();
