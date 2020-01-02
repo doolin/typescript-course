9470017ade167da5a9bf48806921a57a8de8327a
dave doolin <david.doolin@gmail.com>
2020-01-02 03:40:41 -0800

    168, declare full name as an accessor

--- web/src/index.ts
+++ web/src/index.ts
@@ -5,10 +5,10 @@ const user = new User({ id: 1 })
 class Person {
   constructor(public firstName: string, public lastName: string) {}
 
-  fullName(): string {
+  get fullName(): string {
     return `${this.firstName} ${this.lastName}`;
   }
 }
 
 const person = new Person('firstname', 'lastname');
-console.log(person.fullName());
+console.log(person.fullName;
