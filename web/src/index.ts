d14cade05f08db3efda10ff8f9949b426e67d47e
dave doolin <david.doolin@gmail.com>
2020-01-07 03:27:10 -0800

    bound functions

--- web/src/index.ts
+++ web/src/index.ts
@@ -2,15 +2,4 @@ import { User } from './models/User';
 
 const user = new User({ name: 'new record', age: 0 })
 
-// console.log(user.get('name'));
-
-// Reminder on how 'this' works in Javascript
-
-const colors {
-  color: 'red',
-  printColor () {
-    console.log(this.color);
-  }
-};
-
-colors.printColor();
+console.log(user.get('name'));
