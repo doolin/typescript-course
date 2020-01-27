bcbed46b0093c4d5b95e1a3b6564da9c19d013ae
dave doolin <david.doolin@gmail.com>
2020-01-27 03:38:08 -0800

    repeat repeat repeat

--- web/src/models/Collection.ts
+++ web/src/models/Collection.ts
@@ -1,3 +1,11 @@
+import { User } from './User';
+import { Eventing } from './Eventing';
+
 export class Collection {
-  
+  models: User[] = [];
+  events: Eventing = new Eventing();
+
+  get on() {
+    return this.events.on;
+  }
 }
