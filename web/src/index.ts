ee3928c100fa79793a1a1f87760a7372e417112a
dave doolin <david.doolin@gmail.com>
2020-01-31 03:47:10 -0800

    182 grinding through it

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,1 +1,6 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
+
+axios.get('http://localhost:3000/users')
+.then((response: AxiosResponse) => {
+  console.log(response.data);
+});
