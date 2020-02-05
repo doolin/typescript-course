e6ff5ffa4d6bf55900cc5800546a315216ceaaf3
dave doolin <david.doolin@gmail.com>
2020-02-05 03:53:05 -0800

    redoing collection index boring

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,6 +1,5 @@
-import axios, { AxiosResponse } from 'axios';
+import { Collection } from './models/Collection';
 
-axios.get('http://localhost:3000/users')
-.then((response: AxiosResponse) => {
-  console.log(response.data);
-});
+const collection = new Collection(
+  'http://localhost:3000/users'
+);
