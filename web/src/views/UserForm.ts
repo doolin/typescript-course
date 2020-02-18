06554925c942f54292cb7d39b9f334e24c04bdcb
dave doolin <david.doolin@gmail.com>
2020-02-18 03:45:37 -0800

    hating life right now

--- web/src/views/UserForm.ts
+++ web/src/views/UserForm.ts
@@ -1,5 +1,7 @@
+import { User } from '../models/User';
+
 export class UserForm {
-  constructor(public parent: Element) {}
+  constructor(public parent: Element, public model: User) {}
 
   eventsMap(): { [key: string]: () => void } {
     return {
