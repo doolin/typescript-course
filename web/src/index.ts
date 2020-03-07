80fae7c3bc304013668eabd93ab416c81641bd8c
dave doolin <david.doolin@gmail.com>
2020-03-07 04:53:10 -0800

    fixed root element problem

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,15 +1,17 @@
-import { UserForm } from './views/UserForm';
+import { UserEdit } from './views/UserEdit';
 import { User } from './models/User';
 
 const user = User.buildUser({
-  name: 'NAme',
+  name: 'Name',
   age: 20
 });
 
-const root = document.getElementById('#root');
+const root = document.getElementById('root');
 if (root) {
-  const userForm = new UserForm(root, user);
-  userForm.render();
+  const userEdit = new UserEdit(root, user);
+  userEdit.render();
+
+  console.log(userEdit);
 } else {
   throw new Error('Root element not found');
 }
