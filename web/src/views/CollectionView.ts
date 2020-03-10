989c6d199acebfc0a0e19db14051df8fb6af47c9
dave doolin <david.doolin@gmail.com>
2020-03-10 03:56:57 -0700

    207 about half way through

--- web/src/views/CollectionView.ts
+++ web/src/views/CollectionView.ts
@@ -1,2 +1,13 @@
-export abstract class CollectionView {
+import { Collection } from '../models/Collection';
+
+export abstract class CollectionView<T, K> {
+  constructor(public parent: Element, public collection: Collection<T, K>) {}
+
+  abstract renderItem(model: T, itemParent: Element): void;
+
+  render(): void {
+    this.parent.innerHTML = '';
+
+    const templateElement  = document.createElement('template');
+  }
 }
