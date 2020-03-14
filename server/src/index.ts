0c3d0ee6948b3f42d913963e8c3c7150285a3755
dave doolin <david.doolin@gmail.com>
2020-03-14 04:39:27 -0700

    212 basic routes with express complete

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,15 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+app.get('/', (request: Request, response: Response) => {
+  response.send(`
+    <div>
+      <h1>Hi there!</h1>
+    </div>
+  `);
+})
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
