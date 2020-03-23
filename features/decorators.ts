b4c16dce604d589e08ac1ba04f049c0de008ab40
dave doolin <david.doolin@gmail.com>
2020-03-23 04:06:12 -0700

    decorators in typescript, 'tsc --init'

--- features/decorators.ts
+++ features/decorators.ts
@@ -5,7 +5,13 @@ class Boat {
     return `This boat's color is ${this.color}`;
   }
 
+  @testDecorator
   pilot(): void {
     console.log('swish');
   }
+}
+
+function testDecorator(target: any, key: string): void {
+  console.log('Target:', target);
+  console.log('Key: ', key);
 }
