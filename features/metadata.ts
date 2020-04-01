ec77827e24609d1d93086753e232eac88390ef3c
dave doolin <david.doolin@gmail.com>
2020-04-01 04:06:12 -0700

    236 basics of metadata - plane color - 'ts-node metadata.ts'

--- features/metadata.ts
+++ features/metadata.ts
@@ -4,12 +4,17 @@ const plane = {
   color: 'red'
 }
 
-Reflect.defineMetadata('note', 'note metadata', plane);
-Reflect.defineMetadata('height', 10, plane);
-
-console.log(plane);
-
-const note = Reflect.getMetadata('note', plane);
-const height = Reflect.getMetadata('height', plane);
+Reflect.defineMetadata('note', 'note for plane color metadata', plane, 'color');
+const note = Reflect.getMetadata('note', plane, 'color');
 console.log(note);
-console.log(height);
+
+// Reflect.defineMetadata('note', 'note metadata', plane);
+// Reflect.defineMetadata('height', 10, plane);
+
+// console.log(plane);
+
+// const note = Reflect.getMetadata('note', plane);
+// const height = Reflect.getMetadata('height', plane);
+
+// console.log(note);
+// console.log(height);
