24d45e41c6c0b1f6fb131a86b75a7bd3317fc095
dave doolin <david.doolin@gmail.com>
2020-04-08 05:41:37 -0700

    249 automated validation

--- server/src/controllers/decorators/controller.ts
+++ server/src/controllers/decorators/controller.ts
@@ -6,7 +6,19 @@ import { MetadataKeys } from './MetadataKeys';
 
 function bodyValidators(keys: string): RequestHandler {
   return function(req: Request, res: Response, next: NextFunction) {
+    if (!req.body) {
+      res.status(422).send('Invalid request');
+      return
+    }
 
+    for (let key of keys) {
+      if(!req.body[key]) {
+        res.status(422).send('Invalid request');
+        return
+      }
+    }
+
+    next();
   }
 }
 
@@ -23,9 +35,12 @@ export function controller(routePrefix: string) {
           target.prototype,
           key
       ) || [];
+      const requiredBodyProps = Reflect.getMetadata(MetadataKeys.validator, target.prototype, key) || [];
+
+      const validator = bodyValidators(requiredBodyProps);
 
       if (path) {
-        router[method](`${routePrefix}${path}`, ...middlewares, routeHandler);
+        router[method](`${routePrefix}${path}`, ...middlewares, validator, routeHandler);
       }
     }
   };
