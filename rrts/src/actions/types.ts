29a0a53104aa80df5f2e58097e5f06fc8cd08dec
dave doolin <david.doolin@gmail.com>
2020-04-28 03:49:34 -0700

    272 expressing actions as type union

--- rrts/src/actions/types.ts
+++ rrts/src/actions/types.ts
@@ -1,4 +1,8 @@
+import { FetchTodoAction, DeleteTodoAction, FetchTodosAction } from "./todos";
+
 export enum ActionTypes {
   fetchTodos,
   deleteTodo
 }
+
+export type Action = FetchTodosAction | DeleteTodoAction;
